bcee01ba0bb9eb5985b6a013ed06eae7977f0401
LodgyMagdy <lodgymagdy1@gmail.com>
2025-05-03 23:52:50 +0300

    login done

--- Minisocial/src/main/java/recources/UserResource.java
+++ Minisocial/src/main/java/recources/UserResource.java
@@ -2,7 +2,7 @@ package recources;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
 import models.User;
 import service.UserService;
 
@@ -15,5 +15,19 @@ public class UserResource {
     @Inject
     private UserService userService;
 
+    @POST
+    @Path("/login")
+    public Response login(User loginUser) {
+        String userName = loginUser.getUserName();
+        String password = loginUser.getPassword();
 
+        User user = userService.login(userName, password);
+        if (user != null) {
+            return Response.ok("Login successful").build();
+        } else {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("Invalid username or password")
+                    .build();
+        }
+    }
 }
