398b21a97ae1ea51ea8dc48660d684a32ee520d1
Mokhaled2004 <mokhaled2004@gmail.com>
2025-05-03 18:42:06 +0300

    User Service and Recourse Base Created

--- Minisocial/src/main/java/recources/UserResource.java
+++ Minisocial/src/main/java/recources/UserResource.java
@@ -1,53 +1,19 @@
 package recources;
 
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.*;
-
 import javax.ws.rs.core.MediaType;
-
 import models.User;
 import service.UserService;
 
+
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.TEXT_PLAIN) // returning plain strings
+@Produces(MediaType.APPLICATION_JSON) 
 public class UserResource {
 
     @Inject
     private UserService userService;
 
-    @POST
-    @Path("/register")
-    public String createUser(User user) {
-        userService.createUser(user);
-        return "User Created Successfully!";
-    }
 
-    @GET
-    @Path("/{id}")
-    public String getUser(@PathParam("id") int id) {
-        User user = userService.getUserById(id);
-        if (user == null) {
-            return "User not found";
-        }
-        return user.toString();
-    }
-
-    @GET
-    @Path("/all")
-    public String getAllUsers() {
-        List<User> users = userService.getAllUsers();
-
-        if (users.isEmpty()) {
-            return "No users found";
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (User user : users) {
-            sb.append(user.toString()).append("\n");
-        }
-        return sb.toString();
-    }
 }
--- Minisocial/src/main/java/service/UserService.java
+++ Minisocial/src/main/java/service/UserService.java
@@ -1,11 +1,9 @@
 package service;
 
-import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import models.User;
 
 @Stateless
@@ -14,15 +12,5 @@ public class UserService {
 	@PersistenceContext(unitName = "hello")
 	private EntityManager em;
 	
-	public void createUser(User user) {
-		em.persist(user);
-	}
-	
-	public User getUserById(int id) {
-		return em.find(User.class,id);
-	}
-	
-	public List<User> getAllUsers() {
-		return em.createQuery("SELECT u from User u",User.class).getResultList();
-	}
+
 }
