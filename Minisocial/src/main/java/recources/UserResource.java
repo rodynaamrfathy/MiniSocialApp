0ba0bbef05effa9fc0c8ebfbd39f684258b314d7
rodynaamrfathy <rodynaamr@icloud.com>
2025-05-03 19:53:26 +0300

    added logic for user profile managment

--- Minisocial/src/main/java/recources/UserResource.java
+++ Minisocial/src/main/java/recources/UserResource.java
@@ -3,17 +3,31 @@ package recources;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import models.User;
 import service.UserService;
 
-
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON) 
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @Inject
     private UserService userService;
 
-
+    @PUT
+    @Path("/manageprofile")
+    public Response manageProfile(User user) {
+        try {
+            userService.manageProfile(user);
+            return Response.ok().build(); // 200 OK
+        } catch (WebApplicationException e) {
+        	// function btrg3 404 lw user does not exist
+            return Response.status(e.getResponse().getStatus()).entity(e.getMessage()).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                           .entity("Something went wrong: " + e.getMessage())
+                           .build();
+        }
+    }
 }
--- Minisocial/src/main/java/service/UserService.java
+++ Minisocial/src/main/java/service/UserService.java
@@ -4,6 +4,8 @@ package service;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.WebApplicationException;
+
 import models.User;
 
 @Stateless
@@ -11,6 +13,27 @@ public class UserService {
 
 	@PersistenceContext(unitName = "hello")
 	private EntityManager em;
+	
+    //@Inject
+	//private UserTransaction tx;
+
+    
+    // kont ha use UserTransaction begin, commit, rollback bs hya already aslun ejb
+    public void manageProfile(User user) {
+        User existingUserToUpdate = em.find(User.class, user.getUserId());
+
+        if (existingUserToUpdate == null) {
+        	throw new WebApplicationException("User not found", 404);
+        }
+
+        existingUserToUpdate.setFirstName(user.getFirstName());
+        existingUserToUpdate.setLastName(user.getLastName());
+        existingUserToUpdate.setEmail(user.getEmail());
+        existingUserToUpdate.setBirthdate(user.getBirthdate());
+        existingUserToUpdate.setBio(user.getBio());
+        existingUserToUpdate.setPassword(user.getPassword());		
+		
+	}
 	
 
 }
